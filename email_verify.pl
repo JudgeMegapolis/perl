17216d5e04a320ccd5a5ebc55920b8a329d4ec24
Alex <62762106+JudgeMegapolis@users.noreply.github.com>
2022-03-29 05:47:28 -0400

    Add files via upload

---
+++ email_verify.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/env perl -w
+
+use Net::SMTP;
+
+$smtp = Net::SMTP->new("aspmx.l.google.com",
+                        Hello => 'mx1.validemail.com',
+                        Timeout => 60);
+
+my $filename = $ARGV[0];
+my $results = $ARGV[1];
+#open my $out_fd, '>', $results or die $!; # create/open file for writing
+#print $out_fd "test";
+open my $fd, '<', $filename or die $!; # open the file using lexically scoped filehandle
+while( my $email = <$fd>) {
+	print "[*] Checking: ", $email, "\n";
+	$smtp->mail(''); # MAIL FROM:
+	$smtp->recipient($email); # RCPT TO:
+	print "Code returned is: ", $smtp->code(), "\n";
+	if( $smtp->code() == '250' ) {
+		# valid email
+		print "Email is valid\n";
+		open my $out_fd, '>>', $results or die $!; # create/open file for writing
+		print $out_fd $email; # write email to file
+	} elsif( $smtp->code() == '550' ) {
+		# email is invalid
+		print "Email is not valid\n";
+	} else {
+		# something else happened
+		print "SMTP code is ", $smtp->code, "\n";
+	}
+	print "Sleeping\n";
+	sleep(5);
+}
+
+close $out_fd;
+close $fd;
+
